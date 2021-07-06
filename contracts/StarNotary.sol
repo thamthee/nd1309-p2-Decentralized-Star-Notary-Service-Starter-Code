caee5f1023265b0459b065a938fcff575d51c1fe
Tham <thamtheera@gmail.com>
2021-07-06 16:04:53 +0700

    Update smart contract version

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.6.0;
+pragma solidity ^0.8.0;
 
 //Importing openzeppelin-solidity ERC-721 implemented Standard
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
